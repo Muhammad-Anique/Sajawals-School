f81e52d2dd1c66074e4abb4492949d0f79823e60
Muhammad-Anique <anique.cs@gmail.com>
2025-01-22 15:47:25 +0500

    Form Changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,34 @@
-
-export default function Card({ isChecked, plan, selectedPlan, setSelectedPlan }) {
+export default function Card({
+  isChecked,
+  plan,
+  selectedPlan,
+  setSelectedPlan,
+}) {
   function handlePlan(PlanName) {
-    setSelectedPlan(PlanName === selectedPlan ? '' : PlanName);
+    setSelectedPlan(PlanName === selectedPlan ? "" : PlanName);
   }
 
-  return <div className={`card  ${selectedPlan === plan.name ? 'activeCard' : ''}`} onClick={() => handlePlan(plan.name)}>
-    <img style={{width:plan.id === 3 ? "80px" : ""}} src={`${plan.img}`} alt="" />
-    <p>{plan.name}</p>
-    {isChecked ? (
-      <>
-        {/* <div> ${plan.priceYearly}/yr</div>
+  return (
+    <div
+      className={`flex items-center flex-col w-[140px] text-sm h-[100px]  border border-gray-400 rounded-lg hover:bg-gray-100 cursor-pointer p-3  ${
+        selectedPlan === plan.name ? "activeCard" : ""
+      }`}
+      onClick={() => handlePlan(plan.name)}
+    >
+      <img
+        style={{ width: plan.id === 3 ? "50px" : "60px" }}
+        src={`${plan.img}`}
+        alt=""
+      />
+      <p>{plan.name}</p>
+      {isChecked ? (
+        <>
+          {/* <div> ${plan.priceYearly}/yr</div>
         <p className="yearly" div > 2 months free</p> */}
-      </>
-
-    ) : <div>
-      {/* ${plan.priceMonthly}/mo */}
-      </div>}
-
-  </div>
+        </>
+      ) : (
+        <div>{/* ${plan.priceMonthly}/mo */}</div>
+      )}
+    </div>
+  );
 }
--- src/pages/EnterOTP.js
+++ src/pages/EnterOTP.js
@@ -1,13 +1,17 @@
+import React, { useState, useEffect } from "react";
+import OtpInput from "react-otp-input";
 
-import React, { useState,useEffect } from 'react';
-import OtpInput from 'react-otp-input';
-
-const  ThankYou =({otpValue,setOTPValue,handleResend,timeLeft,setTimeLeft}) => {
-  
-     useEffect(() => {
+const ThankYou = ({
+  otpValue,
+  setOTPValue,
+  handleResend,
+  timeLeft,
+  setTimeLeft,
+}) => {
+  useEffect(() => {
     if (timeLeft <= 0) {
       // alert('Time is up!');
-      
+
       return;
     }
 
@@ -17,32 +21,37 @@ const  ThankYou =({otpValue,setOTPValue,handleResend,timeLeft,setTimeLeft}) => {
 
     return () => clearInterval(timerId);
   }, [timeLeft]);
-  return <div className="otp">
-    
-    <div className='otpContainer'>
+  return (
+    <div className="otp">
+      <div className="otpContainer text-black ">
         <OtpInput
-        inputType='tel'
-      value={otpValue}
-      onChange={setOTPValue}
-      numInputs={4}
-      shouldAutoFocus
-      inputStyle={{fontSize:'20px',width:'20px'}}
-      renderSeparator={<span style={{marginLeft:'10px',marginRight:'10px'}}>-</span>}
-      renderInput={(props) => <input {...props} />}
-      
-    />
-    <br/>
-   
+          inputType="tel"
+          value={otpValue}
+          onChange={setOTPValue}
+          numInputs={4}
+          shouldAutoFocus
+          inputStyle={{ fontSize: "20px", width: "50px" }}
+          renderSeparator={
+            <span style={{ marginLeft: "10px", marginRight: "10px" }}>-</span>
+          }
+          renderInput={(props) => <input {...props} />}
+        />
+        <br />
+      </div>
+      <div className="resendContainer">
+        <p style={{ marginLeft: "auto", marginRight: "auto" }}>
+          Haven't Recieved the OTP ?{" "}
+          <button
+            className={`${timeLeft > 0 ? "resendDisabled" : ""}`}
+            disabled={timeLeft > 0}
+            onClick={() => (timeLeft <= 0 ? handleResend() : "")}
+          >
+            Resend {timeLeft > 0 && <>({timeLeft}s)</>}{" "}
+          </button>{" "}
+        </p>
+      </div>
     </div>
-    <div className='resendContainer'>
-      
-         <p style={{marginLeft:'auto',marginRight:'auto'}}>Haven't Recieved the OTP ? <button className={`${timeLeft>0?'resendDisabled':''}`} disabled={timeLeft > 0} onClick={()=> timeLeft<=0 ? handleResend() :''}>Resend {timeLeft >0 && (<>({timeLeft}s)</>)} </button> </p>
-    </div>
-    
-    
-    
+  );
+};
 
-  </div>
-}
-
-export default ThankYou
+export default ThankYou;
