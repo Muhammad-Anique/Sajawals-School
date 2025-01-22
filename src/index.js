742ecc92d4cc281d05da7760d9a14a9da1bbb7cb
Muhammad-Anique <anique.cs@gmail.com>
2025-01-22 15:08:26 +0500

    Deploy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route
+          path="/wp-content/plugins/react-checkout-plugin/build"
+          element={<App />}
+        />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
